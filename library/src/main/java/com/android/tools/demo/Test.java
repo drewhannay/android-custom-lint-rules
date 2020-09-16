0fbdeaec9c7fbe84a592edb381831c4529b83dfe
Tor Norbye <tnorbye@google.com>
2020-09-15 18:20:43 -0700

    More lint results to try to defeat the sticky fingerprints

--- library/src/main/java/com/android/tools/demo/Test.java
+++ library/src/main/java/com/android/tools/demo/Test.java
@@ -3,8 +3,10 @@ package com.android.tools.demo;
 public class Test {
     public void test() {
         String s = "/sdcard/path"; // not allowed
-        String t = "/sdcard/path"; // not allowed
-        String u = "/sdcard/path"; // not allowed
+        String t = "/sdcard/path2"; // not allowed
+        String u = "/sdcard/path3"; // not allowed
+        String v = "/sdcard/path4"; // not allowed
 	Integer i = new Integer(42);
+	Integer j = new Integer(43);
     }
 }

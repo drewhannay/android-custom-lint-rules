1c835c7e46d9c7fe628c14ea9f73f705e1b1ea9b
Tor Norbye <tnorbye@google.com>
2020-09-15 17:42:24 -0700

    Flatten directory structure and add some new lint warnings

--- library/src/main/java/com/android/tools/demo/Test.java
+++ library/src/main/java/com/android/tools/demo/Test.java
@@ -3,6 +3,8 @@ package com.android.tools.demo;
 public class Test {
     public void test() {
         String s = "/sdcard/path"; // not allowed
+        String t = "/sdcard/path"; // not allowed
+        String u = "/sdcard/path"; // not allowed
 	Integer i = new Integer(42);
     }
 }
